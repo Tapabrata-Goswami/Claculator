9bde0d2c5625bb41fa414a7212d63993243d1fcd
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2021-12-04 08:32:03 +0530

    This is most valuable update ---> New options are added

--- Calculator_Code.c
+++ Calculator_Code.c
@@ -1,48 +1,78 @@
 #include <stdio.h>
 
-int main (){
+int main()
+{   
+    printf("\n********TAPABRATA GOSWAMI********\n");
 
-    printf ("This calculator ony for Intiger Number.\n");
+    int choose;
 
-    int a,b,sum;
-    char opretor;
+    printf("\nChoose any one of them. \n\n1. Normal Calculator, 2. Percentage Calculator (1 or 2 ) = ");
 
-    printf("\nChoose a opretor(+,-,*,/) = ");
-    scanf ("%c", &opretor);
+    scanf("%d", &choose);
 
-    printf("\nChoose your first number = ");
-    scanf("%d", &a);
-
-    printf("\nChoose your first number = ");
-    scanf("%d", &b);
-
-
-
-    switch (opretor)
+    if (choose == 1)
     {
-    case '+':
-         sum = (a+b);
-         printf("\nCalculation answer is = %d\n", sum);
-        break;
+        printf("\nThis calculator ony for Intiger Number.\n");
 
-    case '-':
-         sum = (a-b);
-         printf("\nCalculation answer is = %d\n", sum);
-        break;
+        int a, b, sum;
+        char opretor;
 
-    case '*':
-         sum = (a*b);
-         printf("\nCalculation answer is = %d\n", sum);
-        break;
+        printf("\nChoose a opretor(+,-,*,/) = ");
+        scanf("%s", &opretor);
 
-    case '/':
-         sum = (a/b);
-         printf("\nCalculation answer is = %d\n", sum);
-        break;
+        printf("\nChoose your first number = ");
+        scanf("%d", &a);
 
-    default:
-         printf("Something is worng....");
-        break;
+        printf("\nChoose your first number = ");
+        scanf("%d", &b);
+
+        switch (opretor)
+        {
+        case '+':
+            sum = (a + b);
+            printf("\nCalculation answer is = %d\n", sum);
+            break;
+
+        case '-':
+            sum = (a - b);
+            printf("\nCalculation answer is = %d\n", sum);
+            break;
+
+        case '*':
+            sum = (a * b);
+            printf("\nCalculation answer is = %d\n", sum);
+            break;
+
+        case '/':
+            sum = (a / b);
+            printf("\nCalculation answer is = %d\n", sum);
+            break;
+
+        default:
+            printf("Something is worng....");
+            break;
+        }
     }
+
+    else if (choose == 2)
+    {
+        int first_num, secon_num;
+        int sum_per;
+
+        printf("your total value =");
+        scanf("%d", &first_num);
+
+        printf("your obtained value =");
+        scanf("%d", &secon_num);
+
+        sum_per = ((secon_num * 100) / first_num);
+
+        printf("Your Total percentage is = %d\n", sum_per);
+    }
+    else
+    {
+        printf("You are a fuck boy/Girl, Choose a write a number.\n");
+    }
+
     return 0;
 }
